54b852ee7a2cdb2e01ec6550b5ddade788f658e9
imrius <rius.imc@gmail.com>
2024-06-18 12:52:37 -0300

    declaracion de las variables

---
+++ Form1.vb
@@ -0,0 +1,6 @@
+﻿Imports System.Runtime.InteropServices.JavaScript.JSType
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Public Class Form1
+    Dim num1 As Integer
+    Dim salida1, salida2, salida3 As Integer
+End Class
