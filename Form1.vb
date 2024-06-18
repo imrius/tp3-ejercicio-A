4c63fe1f91b24df948571915181631809dde6f9b
imrius <rius.imc@gmail.com>
2024-06-18 13:04:42 -0300

    final

--- Form1.vb
+++ Form1.vb
@@ -5,5 +5,14 @@ Public Class Form1
     Dim salida1, salida2, salida3 As Integer
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         num1 = Integer.Parse(TextBox1.Text)
+
+        salida1 = Math.Truncate(num1 / 100)
+        Label1.Text = "Centena: " & salida1.ToString()
+
+        salida2 = Math.Truncate((num1 Mod 100) / 10)
+        Label2.Text = "Decena: " & salida2.ToString()
+
+        salida3 = num1 Mod 10
+        Label3.Text = "Unidad: " & salida3.ToString()
     End Sub
 End Class
