3fa9f16ece94ee5df03bbd30a0a5260e6ebbc364
imrius <rius.imc@gmail.com>
2024-06-18 13:02:40 -0300

    primer paso

--- Form1.vb
+++ Form1.vb
@@ -3,4 +3,7 @@ Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 Public Class Form1
     Dim num1 As Integer
     Dim salida1, salida2, salida3 As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        num1 = Integer.Parse(TextBox1.Text)
+    End Sub
 End Class
